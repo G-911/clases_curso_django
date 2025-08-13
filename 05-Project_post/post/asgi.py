e2a5302b349873b2132206468072edc971c3fe80
Gabriel Avila <g.avila-2002@hotmail.com>
2025-08-13 14:16:27 -0400

    model post

--- 05-Project_post/post/asgi.py
+++ 05-Project_post/post/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Farmacia project.
+ASGI config for post project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Farmacia.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'post.settings')
 
 application = get_asgi_application()
